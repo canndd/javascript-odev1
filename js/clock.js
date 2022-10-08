967599b26ada088a39a849adf2a05bb14f327380
Can demir <cannddemirr@gmail.com>
2022-10-08 14:18:42 +0300

    saat-tarih bilgisi eklendi

--- js/clock.js
+++ js/clock.js
@@ -2,3 +2,22 @@ const myName= prompt("Adınızı yazınız");
 const Name = document.querySelector("#myName");
 Name.innerHTML=`${myName.length > 2 ? myName : "Kullanıcı adınızı yazmalısınız!"}`;
 
+let days = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+
+
+function showTime() {
+const gün = new Date();
+let h = gün.getHours();
+let d = gün.getMinutes();
+let s = gün.getSeconds();
+let b = days[gün.getDay()];
+let info = document.querySelector("#myClock");
+info.innerHTML=`${h}:${d}:${s} ${b}`;
+}
+setInterval(showTime, 1000);
+
+showTime();
+
+
+
+
